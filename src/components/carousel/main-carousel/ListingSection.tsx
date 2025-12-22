1fade828c168c322f96479ff16228fe06f853171
b166er <delagneauadrien@yahoo.fr>
2025-12-22 16:18:41 +0100

    single responsive

--- src/components/carousel/main-carousel/ListingSection.tsx
+++ src/components/carousel/main-carousel/ListingSection.tsx
@@ -1,9 +1,10 @@
+import { MoveRightIcon } from "lucide-react";
 import Link from "next/link";
 
 import { ListingCard } from "@/actions/listing-actions";
 import { Button } from "@/components/ui/button";
 
-import { ListingCarousel } from "./ListingCarousel"; 
+import { ListingCarousel } from "./ListingCarousel";
 
 type Props = {
   title: string;
@@ -15,13 +16,18 @@ export function ListingsSection({ title, listings, href }: Props) {
   if (!listings.length) return null;
 
   return (
-    <section className="space-y-3 w-full max-w-5xl mx-auto my-4">
+    <section className="mx-auto my-4 w-full max-w-5xl space-y-3">
       {/* Header */}
       <div className="flex items-center justify-between px-1">
         <h2 className="text-xl font-semibold">{title} :</h2>
 
-        <Button asChild variant="link" className="text-sm">
-          <Link href={href}>Voir plus d&apos;annonce</Link>
+        <Button asChild variant="link" className="">
+          <Link href={href} className="group">
+            Voir plus d&apos;annonce{" "}
+            <span>
+              <MoveRightIcon className="group-hover:text-primary "/>
+            </span>
+          </Link>
         </Button>
       </div>
 
