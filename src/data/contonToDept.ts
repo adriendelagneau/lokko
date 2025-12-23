281907221d69aac565e28ee1806ae169f763b13d
b166er <delagneauadrien@yahoo.fr>
2025-12-23 14:57:04 +0100

    search map

---
+++ src/data/contonToDept.ts
@@ -0,0 +1,7 @@
+// src/data/cantonToDept.ts
+export const CANTON_TO_DEPT: Record<string, string> = {
+  "75101": "75",
+  "75102": "75",
+  "92001": "92",
+  // … compléter tous les cantons que tu veux tester
+};
